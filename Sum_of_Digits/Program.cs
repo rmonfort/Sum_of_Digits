b0c828061485882c47922bcb9cb3e212f0fe4fb4
Richard S. Monfort <richard.s.monfort@gmail.com>
2014-09-23 22:15:56 -0400

    Added source and example input file

---
+++ Sum_of_Digits/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sum_of_Digits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] lines = System.IO.File.ReadAllLines(args[0]);
+            foreach (var line in lines)
+            {
+                if (line.Length == 0)
+                {
+                    continue;
+                }
+                int sum = 0;
+                foreach (var digit in line)
+                {
+                    int number = (int)(Char.GetNumericValue(digit));
+                    sum += number;
+                }
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
